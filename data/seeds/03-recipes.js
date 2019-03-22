ff8859ac7ac206675e6f1eceee93e2214f0c907a
dlimla <danielhlim1990@gmail.com>
2019-03-22 00:21:21 -0700

    adds get reqest for dishes, recipes, and ingredients, adds POST request for dishes, adds GET single dish request

--- data/seeds/03-recipes.js
+++ data/seeds/03-recipes.js
@@ -6,7 +6,6 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('recipes').insert([
         {recipe_name: 'Apple_Pan', dish_id:2},
-        {recipe_name: 'BurgerKing', dish_id:2},
         {recipe_name: 'mom_origional', dish_id:1}
       ]);
     });
